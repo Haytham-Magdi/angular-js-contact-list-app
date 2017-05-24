5cbcab3e9cebc1b87dac200cb784a1a1a43ebd31
haytham magdi <haytham.magdi@hotmail.com>
2017-05-24 20:45:23 +0300

    cmt

--- angular-js-contact-list-task-app/server.js
+++ angular-js-contact-list-task-app/server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.get('/mod/*', function (req, res) {
+app.get('/lib/*', function (req, res) {
   var url2 = path.join(__dirname, req.url.replace('mod', 'node_modules'))
   res.sendFile(url2)
 })
